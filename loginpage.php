7c5d7a60b4ea33ff6fe7ba755423aefb9db9130b
kratos93 <mlvnbrandon@gmail.com>
2016-02-01 19:23:34 +0800

    Create loginpage.php

---
+++ loginpage.php
@@ -0,0 +1,32 @@
+
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="admin.css">
+
+</head>
+
+<body>
+
+<nav>
+<ul> 
+  <li style="float:left; padding-left:1%;"><img src="image/tgif.png" height="40" width="110"></li>
+  <li style="font-size:15px";><a href="#about"><b>Sign Up</b></a></li>
+</ul>
+</nav>
+
+<center>
+<div class="divlogin">
+<div class="divform">
+<form action="demo_form.asp" method="get">
+<h1><b>Welcome to Tgif Hotel Reservation System</b></h1>
+<p><b>Username:</b></p>
+<p><input type="text" name="usrname"></p>
+<p><b>Password</b></p>
+<p><input type="text" name="password"></p>
+<p><button>Login</button><button type="reset" value="Reset">Reset</button></p>
+</form>
+</div>
+</div>
+</center>
+</body>
+</html>
