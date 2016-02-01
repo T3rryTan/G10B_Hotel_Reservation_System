e0c6b654848b28e69bccee4bb08886b8f38924fc
T3rryTan <terryrobinsonjr@hotmail.com>
2016-02-01 18:04:09 +0800

    Create mainhotel.php

---
+++ mainhotel.php
@@ -0,0 +1,41 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="mainhotel.css">
+</head>
+<body>
+
+<div class=one><img src="image/tgif-950x950.png" alt="tgif" style="width:180px;height:150px;padding-left:110px";  >
+<input type="button" class="button" value="Sign up">
+
+</div>
+
+
+<div class=two>-EMPTY SPACE-</div>
+
+<div class=nav>
+<p class=Star>Forget the hotel name? Choose Star :</p>
+<select>
+  <option value="none"></option>
+  <option value="one star">one star</option>
+  <option value="two star">two star</option>
+  <option value="three star">three star</option>
+  <option value="four star">four star</option>
+  <option value="five star">five star</option>
+</select>
+</div>
+<div class=section>
+<p>Hotel Pic</p>
+
+</div>
+<div class=footer>
+<h1 class=random>tgif</h1>    
+<h2 class=done>Hotel Reservation System done by:</h2>
+<p class=member>Terry Tan Kim Leng</p>
+<p class=member>Melvin Brandon De Cruz</p>
+<p class=member>Samuel Leong Wei Jian</p>
+<p class=member>Tian Kee</p>
+
+</div>
+
+</body>
+</html>
