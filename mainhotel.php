9363f5269ffda8c7b43a379630da643e8fd811f6
T3rryTan <terryrobinsonjr@hotmail.com>
2016-02-12 23:02:41 +0800

    Update mainhotel.php

--- mainhotel.php
+++ mainhotel.php
@@ -1,32 +1,78 @@
 <html>
 <head>
 <link rel="stylesheet" type="text/css" href="mainhotel.css">
+
 </head>
 <body>
 
-<div class=one><img src="image/tgif-950x950.png" alt="tgif" style="width:180px;height:150px;padding-left:110px";  >
-<input type="button" class="button" value="Sign up">
+<div class=one><img src="image/tgif-950x950.jpg" alt="tgif" style="width:180px;height:150px;padding-left:110px";  >
+<input type="button" class="button" value="Sign out">
+
+</div>
+
+<div>
+<div class="dropdown">
+  <button class="dropbtn">Home</button>
+ 
+</div>
+
+<div class="dropdown">
+  <button class="dropbtn">Dropdown</button>
+  <div class="dropdown-content">
+    <a href="#">Link 1</a>
+    <a href="#">Link 2</a>
+    <a href="#">Link 3</a>
+  </div>
+</div>
+
+<div class="dropdown">
+  <button class="dropbtn">Dropdown</button>
+  <div class="dropdown-content">
+    <a href="#">Link 1</a>
+    <a href="#">Link 2</a>
+    <a href="#">Link 3</a>
+  </div>
+</div>
+
+<div class="dropdown">
+  <button class="dropbtn">Dropdown</button>
+  <div class="dropdown-content">
+    <a href="#">Link 1</a>
+    <a href="#">Link 2</a>
+    <a href="#">Link 3</a>
+  </div>
+</div>
+
+<div class="dropdown">
+  <button class="dropbtn">Dropdown</button>
+  <div class="dropdown-content">
+    <a href="#">Link 1</a>
+    <a href="#">Link 2</a>
+    <a href="#">Link 3</a>
+  </div>
+</div>
+
+<div class="dropdown">
+  <button class="dropbtn">Dropdown</button>
+  <div class="dropdown-content">
+    <a href="#">Link 1</a>
+    <a href="#">Link 2</a>
+    <a href="#">Link 3</a>
+  </div>
+</div>
 
 </div>
 
 
-<div class=two>-EMPTY SPACE-</div>
 
-<div class=nav>
-<p class=Star>Forget the hotel name? Choose Star :</p>
-<select>
-  <option value="All"></option>
-  <option value="one star">one star</option>
-  <option value="two star">two star</option>
-  <option value="three star">three star</option>
-  <option value="four star">four star</option>
-  <option value="five star">five star</option>
-</select>
-</div>
 <div class=section>
-<div class="scroll">Pic hotel 
-</div>
-
+<div>
+		<form>
+			<p><img src="image/HOTEL_2323685b.jpg" style="margin-left:120px; width: 80%; height: 350px" alt="image" </a></p>
+			<p><img style="width: 33.2%; height: 200px" alt="image" src="image/holiday-inn.jpg"/><img style="width: 33.2%; height: 200px" alt="image" src="image/hatten-hotel-melaka.jpg"/>
+			<img style="width: 33.2%; height: 200px" alt="image" src="image/Holiday_Inn_-_1-1000x600.jpg"/></p>
+		</form>
+	</div>
 </div>
 <div class=footer>
 <h1 class=tgif>tgif</h1>
