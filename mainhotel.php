f79301c13d00b7e5cecfb14c55d0fbbfbb112d9c
T3rryTan <terryrobinsonjr@hotmail.com>
2016-02-13 23:45:33 +0800

    Update mainhotel.php

--- mainhotel.php
+++ mainhotel.php
@@ -4,71 +4,13 @@
 
 </head>
 <body>
-
-<div class=one><img src="image/tgif-950x950.jpg" alt="tgif" style="width:180px;height:150px;padding-left:110px";  >
-<input type="button" class="button" value="Sign out">
-
-</div>
-
-<div>
-<div class="dropdown">
-  <button class="dropbtn">Home</button>
- 
-</div>
-
-<div class="dropdown">
-  <button class="dropbtn">Dropdown</button>
-  <div class="dropdown-content">
-    <a href="#">Link 1</a>
-    <a href="#">Link 2</a>
-    <a href="#">Link 3</a>
-  </div>
-</div>
-
-<div class="dropdown">
-  <button class="dropbtn">Dropdown</button>
-  <div class="dropdown-content">
-    <a href="#">Link 1</a>
-    <a href="#">Link 2</a>
-    <a href="#">Link 3</a>
-  </div>
-</div>
-
-<div class="dropdown">
-  <button class="dropbtn">Dropdown</button>
-  <div class="dropdown-content">
-    <a href="#">Link 1</a>
-    <a href="#">Link 2</a>
-    <a href="#">Link 3</a>
-  </div>
-</div>
-
-<div class="dropdown">
-  <button class="dropbtn">Dropdown</button>
-  <div class="dropdown-content">
-    <a href="#">Link 1</a>
-    <a href="#">Link 2</a>
-    <a href="#">Link 3</a>
-  </div>
-</div>
-
-<div class="dropdown">
-  <button class="dropbtn">Dropdown</button>
-  <div class="dropdown-content">
-    <a href="#">Link 1</a>
-    <a href="#">Link 2</a>
-    <a href="#">Link 3</a>
-  </div>
-</div>
-
-</div>
-
-
-
+<?php
+include("menu.php");
+?>
 <div class=section>
 <div>
 		<form>
-			<p><img src="image/HOTEL_2323685b.jpg" style="margin-left:120px; width: 80%; height: 350px" alt="image" </a></p>
+			<p><img src="image/HOTEL_2323685b.jpg" style=" width: 100%; height: 350px" alt="image" </a></p>
 			<p><img style="width: 33.2%; height: 200px" alt="image" src="image/holiday-inn.jpg"/><img style="width: 33.2%; height: 200px" alt="image" src="image/hatten-hotel-melaka.jpg"/>
 			<img style="width: 33.2%; height: 200px" alt="image" src="image/Holiday_Inn_-_1-1000x600.jpg"/></p>
 		</form>
