c47fb3e8b3187294d4e840bc0f44d4c4cc43dc32
T3rryTan <terryrobinsonjr@hotmail.com>
2016-02-13 23:56:59 +0800

    Update contactus.php

--- contactus.php
+++ contactus.php
@@ -1,5 +1,4 @@
 <?php 
-/* ----------- CONTACT PAGE ----------------*/
 include("dataconn.php");
 ?>
 
@@ -15,10 +14,11 @@ include("dataconn.php");
 <div class=conbg>
 <div>
 	<h1 class=about>Contact Us</h1>
-	<h3 class=about>If got any questions, Please contact us down below, We are all ear.</h3>
+	<h3 class=about>If got any questions, Please contact us down below, We are all hear.</h3>
 	<p class=about>Email:terryrobinsonjr@hotmail.com</p>
-	<p class=about>athrun.tian@gmail.com <br>Melvin<br></p>
-	<p class=about>contact number:<br>0177401800<br>0126213018</p>
+	<p class=about>athrun.tian@gmail.com <br>Melvin<br>Samuel</p>
+	<p class=about>or you can call us :</p>
+	<p class=about>Contact Number:<br>Terry Tan: 0126213018<br>Tian Kee: 0177401800<br>Melvin Brandon De Cruz: 0188725855<br> Samuel Leong: 0106611760</p>	
 	<p class=about>Thank you.</p>	
 </div>
 
