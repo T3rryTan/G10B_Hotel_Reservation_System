c1ee92add3a755a253fcca9cd7eed67a92b12cca
T3rryTan <terryrobinsonjr@hotmail.com>
2016-02-13 23:46:09 +0800

    Create contactus.php

---
+++ contactus.php
@@ -0,0 +1,28 @@
+<?php 
+/* ----------- CONTACT PAGE ----------------*/
+include("dataconn.php");
+?>
+
+<html>
+<head>
+    <link href="mainhotel.css" rel="stylesheet" type="text/css"/>
+</head>
+<body>
+<?php
+    include("menu.php");
+?>
+
+<div class=conbg>
+<div>
+	<h1 class=about>Contact Us</h1>
+	<h3 class=about>If got any questions, Please contact us down below, We are all ear.</h3>
+	<p class=about>Email:terryrobinsonjr@hotmail.com</p>
+	<p class=about>athrun.tian@gmail.com <br>Melvin<br></p>
+	<p class=about>contact number:<br>0177401800<br>0126213018</p>
+	<p class=about>Thank you.</p>	
+</div>
+
+
+	
+</body>		
+</html>
