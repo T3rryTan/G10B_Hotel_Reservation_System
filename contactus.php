ff92def1d593e93bd5d0699f446e71664b1df6f8
T3rryTan <terryrobinsonjr@hotmail.com>
2016-02-15 02:06:17 +0800

    Update contactus.php

--- contactus.php
+++ contactus.php
@@ -1,4 +1,5 @@
 <?php 
+/* ----------- CONTACT PAGE ----------------*/
 include("dataconn.php");
 ?>
 
@@ -14,15 +15,54 @@ include("dataconn.php");
 <div class=conbg>
 <div>
 	<h1 class=about>Contact Us</h1>
-	<h3 class=about>If got any questions, Please contact us down below, We are all hear.</h3>
-	<p class=about>Email:terryrobinsonjr@hotmail.com</p>
-	<p class=about>athrun.tian@gmail.com <br>Melvin<br>Samuel</p>
-	<p class=about>or you can call us :</p>
-	<p class=about>Contact Number:<br>Terry Tan: 0126213018<br>Tian Kee: 0177401800<br>Melvin Brandon De Cruz: 0188725855<br> Samuel Leong: 0106611760</p>	
-	<p class=about>Thank you.</p>	
+	<h3 class=about>If got any questions, Please contact us down below</h3>
 </div>
-
-
-	
+<br>
+<table style="width:100%">
+  <tr>
+    <th>Name</th>
+    <th>Postion</th>		
+    <th>Contact Number</th>
+	<th>Email</th>
+  </tr>
+  
+  <tr>
+    <td>Terry Tan Kim Leng</td>
+    <td>Group Leader</td>		
+    <td>0126213018</td>
+	<td><a href=mailto:terryrobinsonjr@hotmail.com>terryrobinsonjr@hotmail.com</a></td>
+  </tr>
+  
+  <tr>
+    <td>Kee Chen Thiam</td>		
+    <td>System Manager</td>
+	<td>0177401800</td>
+    <td><a href=mailto:athrun.tian@gmail.com>athrun.tian@gmail.com</a></td> 
+  </tr>
+  
+  <tr>
+    <td>Melvin Brandon De Cruz</td>
+    <td>Security Manager</td>		
+    <td>0188725855</td>
+	<td><a href=mailto:mlvnbrandon@gmail.com>mlvnbrandon@gmail.com</a></td>
+  </tr>
+  
+  <tr>
+    <td>Samuel Leong Wei Jian</td>
+    <td>Interface Designer</td>		
+    <td>0106611760</td>
+	<td><a href=mailto:slwj_5319@hotmail.com>slwj_5319@hotmail.com</a></td>
+  </tr>
+</table>
+<br>
+<address class=about>
+Visit us at:<br>
+tgif HQ<br>
+Jalan Ayer Keroh Lama,<br> 
+75450 Melaka, <br>
+Malaysia<br>
+</address>
+<p class=about>Thank you.</p>
+<p class=about>We will hear your problem and suggestion.	
 </body>		
 </html>
