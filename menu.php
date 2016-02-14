b1443c6aedae0f3edeae4f9f9cd5333063db7ce2
T3rryTan <terryrobinsonjr@hotmail.com>
2016-02-15 02:08:55 +0800

    Update menu.php

--- menu.php
+++ menu.php
@@ -6,30 +6,27 @@ $categoryTypeQuery = "SELECT * FROM hotel_type";
 $categoryResult = mysql_query($categoryTypeQuery) or die(mysql_error());
 
 ?>
-<div class=one><img src="image/tgif-950x950.jpg" alt="tgif" style="width:180px;height:150px;">
-
-
-</div>
+<div class=one><img src="image/tgif-950x950.jpg" alt="tgif" style="width:180px;height:150px;"></div>
 <div id='cssmenu'>
   <ul>
-        <li><a <?php echo ($current_path == 'mainhotel.php' ? 'class="active"' : '') ?> href='mainhotel.php'><span>Home</span></a></li>
+        <li><a <?php echo ($current_path == 'mainhotel.php' ? 'class="active"' : '') ?> href='mainhotel.php'><span>HOME</span></a></li>
         <?php
         if(!isset($_SESSION["login"]) || $_SESSION["login"] == false)
 		{
         ?>
-            <li><a <?php echo ($current_path == 'login.php' ? 'class="active"' : '') ?> href='login.php'><span>Login</span></a></li>
-            <li><a <?php echo ($current_path == 'register.php' ? 'class="active"' : '') ?> href='register.php'><span>Register</span></a></li>
+            <li><a <?php echo ($current_path == 'login page.php' ? 'class="active"' : '') ?> href='login page.php'><span>LOGIN</span></a></li>
+            <li><a <?php echo ($current_path == 'register.php' ? 'class="active"' : '') ?> href='register.php'><span>REGISTER</span></a></li>
         <?php
         }
         ?>
-
-        <li <?php echo ($current_path == 'category' ? 'class="active"' : '') ?> class='has-sub'><a href='#'><span>Bill</span></a>
+		<li><a <?php echo ($current_path == 'hotel.php' ? 'class="active"' : '') ?> href='hotel.php'><span>HOTEL</span></a></li>
+        <li <?php echo ($current_path == 'room' ? 'class="active"' : '') ?> class='has-sub'><a href='#'><span>ROOM</span></a>
             <ul>
                 <?php
                 while($row = mysql_fetch_assoc($categoryResult))
 				{
                     //var_dump($category);
-                    echo "<li class='has-sub'><a href='category0.php?id=".$row["hotelTypeID"]."'><span>".$row["hotelTypeName"]."</span></a></li>";
+                    echo "<li class='has-sub'><a href='room.php?id=".$row["hotelTypeID"]."'><span>".$row["hotelTypeName"]."</span></a></li>";
                 }
                 ?>
 				
@@ -39,7 +36,8 @@ $categoryResult = mysql_query($categoryTypeQuery) or die(mysql_error());
         <?php
         if(isset($_SESSION["login"]) && $_SESSION["login"]){
         ?>
-            <li><a <?php echo ($current_path == 'order.php' ? 'class="active"' : '') ?> href='order.php'><span>Order Status</span></a></li>
+            <li><a <?php echo ($current_path == 'payment.php' ? 'class="active"' : '') ?> href='payment.php'><span>Payment</span></a></li>
+			<li><a <?php echo ($current_path == 'order.php' ? 'class="active"' : '') ?> href='order.php'><span>Order Status</span></a></li>
             <li><a <?php echo ($current_path == 'edit.php' ? 'class="active"' : '') ?> href='edit.php'><span>Edit Profile</span></a></li>
         <?php
         }
@@ -50,9 +48,10 @@ $categoryResult = mysql_query($categoryTypeQuery) or die(mysql_error());
         <?php
         if(isset($_SESSION["login"]) && $_SESSION["login"]){
         ?>
-            <li><a href='logout.php'><span>Logout</span></a></li>
+            <li ><a href='logout.php'><span class=logo>LOGOUT</span></a></li>
         <?php
         }
         ?>
     </ul>
 </div>
+
