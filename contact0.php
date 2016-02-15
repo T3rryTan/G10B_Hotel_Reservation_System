eff1404d1b08e782f21b10afe85eea477637526c
T3rryTan <terryrobinsonjr@hotmail.com>
2016-02-15 15:42:42 +0800

    Create contact0.php

---
+++ contact0.php
@@ -0,0 +1,68 @@
+<?php 
+/* ----------- CONTACT PAGE ----------------*/
+include("dataconn.php");
+?>
+
+<html>
+<head>
+    <link href="mainhotel.css" rel="stylesheet" type="text/css"/>
+</head>
+<body>
+<?php
+    include("menu0.php");
+?>
+
+<div class=conbg>
+<div>
+	<h1 class=about>Contact Us</h1>
+	<h3 class=about>If got any questions, Please contact us down below</h3>
+</div>
+<br>
+<table style="width:100%">
+  <tr>
+    <th>Name</th>
+    <th>Postion</th>		
+    <th>Contact Number</th>
+	<th>Email</th>
+  </tr>
+  
+  <tr>
+    <td>Terry Tan Kim Leng</td>
+    <td>Group Leader</td>		
+    <td>0126213018</td>
+	<td><a href=mailto:terryrobinsonjr@hotmail.com>terryrobinsonjr@hotmail.com</a></td>
+  </tr>
+  
+  <tr>
+    <td>Kee Chen Thiam</td>		
+    <td>System Manager</td>
+	<td>0177401800</td>
+    <td><a href=mailto:athrun.tian@gmail.com>athrun.tian@gmail.com</a></td> 
+  </tr>
+  
+  <tr>
+    <td>Melvin Brandon De Cruz</td>
+    <td>Security Manager</td>		
+    <td>0188725855</td>
+	<td><a href=mailto:mlvnbrandon@gmail.com>mlvnbrandon@gmail.com</a></td>
+  </tr>
+  
+  <tr>
+    <td>Samuel Leong Wei Jian</td>
+    <td>Interface Designer</td>		
+    <td>0106611760</td>
+	<td><a href=mailto:slwj_5319@hotmail.com>slwj_5319@hotmail.com</a></td>
+  </tr>
+</table>
+<br>
+<address class=about>
+Visit us at:<br>
+tgif HQ<br>
+Jalan Ayer Keroh Lama,<br> 
+75450 Melaka, <br>
+Malaysia<br>
+</address>
+<p class=about>Thank you.</p>
+<p class=about>We will hear your problem and suggestion.	
+</body>		
+</html>
