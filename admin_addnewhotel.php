de67847b853e5175005469a119a117f4191bce53
samuel5319 <slwj_5319@hotmail.com>
2016-02-15 15:32:32 +0800

    Update admin_addnewhotel.php

--- admin_addnewhotel.php
+++ admin_addnewhotel.php
@@ -1,14 +1,18 @@
 <?php
 /* ----------- ADMIN  ADD PAGE ----------------*/
 include("dataconn.php");
+
 if (!isset($_SESSION["adminID"]) || $_SESSION["adminID"] == null) {
     die("You are not admin, you cannot access this page");
 }
+
 $action = isset($_GET['action']) ? $_GET['action'] : 'add';
+
 if (isset($_POST["add"])) {
     
     $hotelTypeName       = $_POST["hotelTypeName"];
-    
+	$hotelTypePhone       = $_POST["hotelTypePhone"];
+    $hotelTypeAddress       = $_POST["hotelTypeAddress"];
     $allowedExts = array(
         "gif",
         "jpeg",
@@ -17,6 +21,7 @@ if (isset($_POST["add"])) {
     );
     $temp        = explode(".", $_FILES["file"]["name"]);
     $extension   = end($temp);
+
     if ((($_FILES["file"]["type"] == "image/gif") || ($_FILES["file"]["type"] == "image/jpeg") || ($_FILES["file"]["type"] == "image/jpg") || ($_FILES["file"]["type"] == "image/pjpeg") || ($_FILES["file"]["type"] == "image/x-png") || ($_FILES["file"]["type"] == "image/png")) && in_array($extension, $allowedExts)) {
         if ($_FILES["file"]["error"] > 0) {
             echo "Error: " . $_FILES["file"]["error"] . "<br>";
@@ -25,11 +30,14 @@ if (isset($_POST["add"])) {
             echo "Type: " . $_FILES["file"]["type"] . "<br>";
             echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
             echo "Stored in: " . $_FILES["file"]["tmp_name"];
+
 			$path = "image/";
 			if (!is_dir($path)) {
 				mkdir($path, 0777, true);
 			}
+
 			$hotelTypeImage = $path . "/" . $_FILES["file"]["name"];
+
             if (file_exists($hotelTypeImage)) {
                 echo $_FILES["file"]["name"] . " already exists. ";
             } else {
@@ -37,8 +45,9 @@ if (isset($_POST["add"])) {
                 echo "Stored in: " . $hotelTypeImage;
             }
         }
+
         $db_image_path ='/'.$_FILES["file"]["name"]; // without -> image/ <- folder for db
-        mysql_query("INSERT INTO hotel_type(hotelTypeName, hotelTypeImage) VALUES ('$hotelTypeName', '$db_image_path')") or die(mysql_error());
+        mysql_query("INSERT INTO hotel_type(hotelTypeName,hotelTypePhone,hotelTypeAddress,hotelTypeImage) VALUES ('$hotelTypeName','$hotelTypePhone','$hotelTypeAddress','$db_image_path')") or die(mysql_error());
     	$_SESSION['flash_msg'] = "Item Added";
     } else {
         echo "Invalid file";
@@ -48,7 +57,8 @@ if (isset($_POST["add"])) {
 
 <html>
 <head>
-    <link href="addnewhotel.css" rel="stylesheet" type="text/css"/>
+	<link rel="stylesheet" type="text/css" href="mainhotel.css">
+    <link href="css.css" rel="stylesheet" type="text/css"/>
 </head>
 <body>
 
@@ -70,40 +80,38 @@ if (isset($_POST["add"])) {
 
     <!-- ======== ADD ========== -->
     <?php if($action == "add") { ?>
-		
-<div class=one><img src="image/tgif-950x950.png" alt="tgif" style="width:180px;height:150px;"  >
-</div>		
-		<p class=name1>Add New Hotel</p>
-		
-		
-		<div class=two>
+		<p><span style="font-size: 30px; font-weight: bold;">Add New Hotel</span>
+
+		<br><div>
 	      <label for="hotelTypeName">New Hotel Name :</label><br>
-	      <input type="text" id="hotelTypeName" name="hotelTypeName" size="30" placeholder="Enter item name" />
+	      <input type="text" id="hotelTypeName" name="hotelTypeName" size="50" placeholder="Enter hotel name" />
 	    </div>
-		<br>
+		<br><div>
+	      <label for="hotelTypeName">New Hotel Phone :</label><br>
+	      <input type="numeber" id="hotelTypePhone" name="hotelTypePhone" size="10" placeholder="Enter hotel address" />
+	    </div>
+		<br><div>
+	      <label for="hotelTypeName">New Hotel address :</label><br>
+	      <input type="text" id="hotelTypeAddress" name="hotelTypeAddress" size="150" placeholder="Enter hotel address" />
+	    </div>
 		
-		<div class=file1>
+
+		<br><div>
 	        <label for="file">Image:</label>
 			<input type="file" name="file" id="file">
-			
 		</div>
-		<br>
-		<div class=submit1>
+		<br><div class="submit">
 	      <input type="submit" name="add" class="custom-button" value="Add" />
-			<button type="reset" value="Reset">Reset</button>
-		</div>
-		</br>
+	    </div></br>
       <?php } ?>
   <!-- ======== ADD ========== -->
 
   
   <!-- ======== UPDATE ========== -->
   <?php if($action == "update") { ?>
-  <p class=update1>Update Product</p>
+  <p><span style="font-size: 30px; font-weight: bold;">Update Product</span>
   <?php } ?>
   <!-- ======== UPDATE ========== -->
 
 	</fieldset>
 </form>
-</body>
-</html>
